8e5fba04ad218706778fe2c3e6f66ced8dcc2fac
dinabogdan <bogdan.dina03@gmail.com>
2020-05-09 17:51:51 +0300

    handle invalid values

--- src/main/kotlin/com/freesoft/arrowplayground/Main.kt
+++ src/main/kotlin/com/freesoft/arrowplayground/Main.kt
@@ -1,13 +1,13 @@
 package com.freesoft.arrowplayground
 
-import arrow.core.Failure
-import arrow.core.Success
 import arrow.core.Try
 import java.security.SecureRandom
 
 private val random = SecureRandom()
 
 fun parseInt(input: String?) = Try { input?.toInt() }
+
+private fun String.safeToInt() = Try { this.toInt() }
 
 fun main() {
 
@@ -18,19 +18,29 @@ fun main() {
     while (exec) {
         val number = random.nextInt(5) + 1
         println("Dear $name, please guess a number from 1 to 5: ")
-        val guess = parseInt(readLine())
-        when (guess) {
-            is Failure -> println("You did not enter a number!")
-            is Success<Int?> -> {
-                if (guess.value == number) println("You guessed right, $name")
-                else println("You guessed wrong, $name! The number was $number")
+        evaluate(number, name)
+        println("Do you want to continue, $name?")
+        var cont = true
+        while (cont) {
+            when (readLine()?.toLowerCase()) {
+                "y" -> exec = true
+                "n" -> exec = false
+                else -> cont = true
             }
         }
-        println("Do you want to continue, $name?")
-        when (readLine()) {
-            "y" -> exec = true
-            "n" -> exec = false
-        }
+
     }
     println("Hello world!")
+}
+
+private fun evaluate(number: Int, name: String?) {
+    (readLine() as String)
+            .safeToInt()
+            .fold(
+                    { println("You did not enter a number!") },
+                    {
+                        if (it == number) println("You guessed right, $name")
+                        else println("You guessed wrong, $name! The number was $number")
+                    }
+            )
 }
