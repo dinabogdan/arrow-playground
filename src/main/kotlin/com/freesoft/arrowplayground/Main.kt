147972475034c6c0d02281e469969fe07772aade
dinabogdan <bogdan.dina03@gmail.com>
2020-05-09 19:03:56 +0300

    refactor

--- src/main/kotlin/com/freesoft/arrowplayground/Main.kt
+++ src/main/kotlin/com/freesoft/arrowplayground/Main.kt
@@ -2,53 +2,55 @@ package com.freesoft.arrowplayground
 
 import arrow.core.Try
 import arrow.effects.IO
+import arrow.effects.fix
+import arrow.effects.instances.io.monad.binding
+import arrow.effects.instances.io.monad.monad
+import arrow.effects.liftIO
 import java.security.SecureRandom
 
 private val random = SecureRandom()
 
-fun parseInt(input: String?) = Try { input?.toInt() }
-
-private fun String.safeToInt() = Try { this.toInt() }
+private fun IO<String?>.safeToInt() = Try { this.map { it?.toInt() } }
 
 fun main() {
-
-    println("What is your name?")
-    val name = readLine()
-    println("Hello, $name, welcome to the game")
-    var exec = true
-    while (exec) {
-        val number = random.nextInt(5) + 1
-        println("Dear $name, please guess a number from 1 to 5: ")
-        evaluate(number, name)
-        checkContinue(name)
-    }
+    mainIO().unsafeRunSync()
 }
 
-private fun evaluate(number: Int, name: String?) {
-    (readLine() as String)
-            .safeToInt()
-            .fold(
-                    { println("You did not enter a number!") },
-                    {
-                        if (it == number) println("You guessed right, $name")
-                        else println("You guessed wrong, $name! The number was $number")
-                    }
-            )
+private fun mainIO(): IO<Unit> = binding {
+    putStrLn("What is your name?").bind()
+    val name = readStrLn().bind()
+    putStrLn("Hello, $name, welcome to the game").bind()
+    gameLoop(name).bind()
 }
 
-private fun checkContinue(name: String?): Boolean {
-    println("Do you want to continue, $name?")
-    return (readLine() as String).transform { it.toLowerCase() }
-            .transform {
+private fun gameLoop(name: String?): IO<Unit> = IO.monad().binding {
+    putStrLn("Dear $name, please guess a number from 1 to 5: ").bind()
+    readStrLn().safeToInt().fold(
+            { putStrLn("You did not enter a number!").bind() },
+            {
+                val number = nextInt(5).map { it + 1 }.bind()
+                if (it.bind() == number) putStrLn("You guessed right, $name")
+                else putStrLn("You guessed wrong, $name! The number was $number").bind()
+            }
+    )
+    checkContinue(name).map {
+        (if (it) gameLoop(name) else Unit.liftIO())
+    }.flatten().bind()
+}.fix()
+
+private fun checkContinue(name: String?): IO<Boolean> = IO.monad().binding {
+    putStrLn("Do you want to continue, $name?").bind()
+    (readStrLn()).map { it?.toLowerCase() }
+            .map {
                 when (it) {
-                    "y" -> true
-                    "n" -> false
+                    "y" -> true.liftIO()
+                    "n" -> false.liftIO()
                     else -> checkContinue(name)
                 }
-            }
-}
+            }.flatten()
+            .bind()
+}.fix()
 
-private fun <T> String.transform(f: (String) -> T) = f(this)
-
+private fun nextInt(upper: Int): IO<Int> = IO { random.nextInt() }
 private fun putStrLn(line: String): IO<Unit> = IO { println(line) }
 private fun readStrLn(): IO<String?> = IO { readLine() }
