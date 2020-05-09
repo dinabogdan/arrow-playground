3a9d7e5a49adc652a5960075ce52c554f03ca916
dinabogdan <bogdan.dina03@gmail.com>
2020-05-09 17:37:38 +0300

    imperative way

--- src/main/kotlin/com/freesoft/arrowplayground/Main.kt
+++ src/main/kotlin/com/freesoft/arrowplayground/Main.kt
@@ -1,5 +1,27 @@
 package com.freesoft.arrowplayground
 
+import java.security.SecureRandom
+
+private val random = SecureRandom()
+
 fun main() {
+
+    println("What is your name?")
+    val name = readLine()
+    println("Hello, $name, welcome to the game")
+    var exec = true
+    while (exec) {
+        val number = random.nextInt(5) + 1
+        println("Dear $name, please guess a number from 1 to 5: ")
+        val guess = readLine()?.toInt()
+        if (guess == number)
+            println("You guessed right, $name")
+        else println("You guessed wrong, $name! The number was $number")
+        println("Do you want to continue, $name?")
+        when (readLine()) {
+            "y" -> exec = true
+            "n" -> exec = false
+        }
+    }
     println("Hello world!")
 }
